4abfe4a35decaa025b803927efd44494183d5cc4
yunzhil <yunzhi.lin@gatech.edu>
2021-07-08 13:23:33 -0400

    change param input for functions

--- testing/testBasic01_usage.py
+++ testing/testBasic01_usage.py
@@ -1,12 +1,12 @@
-#=============================== testBasic ==============================
+#=============================== testBasic01_usage ==============================
 #
 # @brief    Code to create an image interface and then use it to process
 #           the source image
 #
-#=============================== testBasic ==============================
+#=============================== testBasic01_usage ==============================
 
 #
-# @file     testBasic.py
+# @file     testBasic01_usage.py
 #
 # @author   Yunzhi Lin, yunzhi.lin@gatech.edu
 # @date     2021/07/07 [created]
@@ -16,7 +16,7 @@
 #!  Tab is set to 4 spaces with conversion to spaces.
 #
 # @quit
-#=============================== testBasic ==============================
+#=============================== testBasic01_usage ==============================
 
 #==[0] Prep environment
 #
@@ -28,7 +28,7 @@ newImage_list = []
 
 #==[1] Create the interface class, you can also try any opencv functions based on the needs
 #
-improc = improcessor.basic('resize',(400,10),'clip',(2,100))
+improc = improcessor.basic('resize',((400,10),),'clip',((2,100),))
 
 #==[2] Apply the methods
 #
@@ -37,7 +37,7 @@ newImage_list.append(improc.apply(Image))
 #==[3] Reset the methods
 #
 # improc.set('ignore',True)
-improc.set('processing',['clip',(2,20)])
+improc.set('processing',['clip',((2,20),)])
 newImage_list.append(improc.apply(Image))
 
 #==[4] Directly apply the builtin methods
