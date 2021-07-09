807672f1f4ab4f84ad6ab7a859009cee25e78803
Patricio A. Vela <pvela@gatech.edu>
2021-07-08 23:12:06 -0400

    added new simpler basic test.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='improcessor',
+      version='1.0',
+      description='Classes implementing flexible and run-time specifiable image processing pipelines.', 
+      author='IVALab',
+      packages=['improcessor']
+      )
---
+++ testing/testBasic01_usage.py
@@ -0,0 +1,77 @@
+#=============================== testBasic02_threshold ==============================
+#
+# @brief    Code to create an image interface and then use it to process
+#           the source image (threshold)
+#
+#=============================== testBasic02_threshold ==============================
+
+#
+# @file     testBasic02_threshold.py
+#
+# @author   Yunzhi Lin, yunzhi.lin@gatech.edu
+# @date     2021/07/08 [created]
+#
+#!NOTE:
+#!  Indent is set to 2 spaces.
+#!  Tab is set to 4 spaces with conversion to spaces.
+#
+# @quit
+#=============================== testBasic02_threshold ==============================
+
+#==[0] Prep environment
+#
+import numpy as np
+import operator
+import improcessor.basic as improcessor
+
+image = np.zeros((10,10))
+image[1:4,4:7] = 10
+
+#==[1] Create interface class and apply the method.
+#
+improc = improcessor.basic(operator.ge,(7,))
+outIm  = improc.apply(image)
+
+print("True values where 10 values were:\n")
+print(outIm)
+
+
+#==[2] Override with new image processing: scale.
+
+scVals = np.array([0, 1])
+improc.set('processing', improcessor.basic.scale, (scVals,))
+
+outIm = improc.apply(image)
+
+print("1 values where 10 values were:\n")
+print(outIm)
+
+#==[3] Override with new image processing: scaleabout.
+
+scVals = np.array([0, 5])
+improc.set('processing', improcessor.basic.scaleabout, (scVals,))
+
+outIm = improc.apply(image)
+
+print("2.5 values where 10 values were, -2.5 values where 0 values were:\n")
+print(outIm)
+
+#==[4] Override with new image processing: normalize.
+
+improc.set('processing', improcessor.basic.normalize, ())
+
+outIm = improc.apply(image)
+
+print("1 values where 10 values were:\n")
+print(outIm)
+
+#==[5] Override with new image processing: clip.
+
+cVals = np.array([2, 8])
+improc.set('processing', improcessor.basic.clip, (cVals,))
+
+outIm = improc.apply(image)
+
+print("2/8 values where 0/10 values were:\n")
+print(outIm)
+
