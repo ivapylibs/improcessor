e068aa6f6ff80548169b206d70a4bd4f03b5d49f
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-02-17 16:19:58 -0500

    Use find_packages in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
-setup(name='improcessor',
-      version='1.0',
-      description='Classes implementing flexible and run-time specifiable image processing pipelines.', 
-      author='IVALab',
-      packages=['improcessor'],
-      install_requires=['numpy', 'matplotlib', 'opencv-contrib-python', 'scikit-image'],
-      )
+from setuptools import setup, find_packages
+
+setup(
+    name="improcessor",
+    version="1.0.1",
+    description="Classes implementing flexible and run-time specifiable image processing pipelines.",
+    author="IVALab",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "opencv-contrib-python",
+        "scikit-image",
+    ],
+)
